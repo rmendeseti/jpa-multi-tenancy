92be2f75799b39f0a28008bdd75aafea2116d51e
Nildo Mendes <rmendeseti@gmail.com>
2020-11-19 07:38:21 -0400

    Alterando o banco para postgresql

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,5 @@
+create table ULTRAHM.usuario(id number, name varchar(200), email varchar(200));
+create table SICREDIHM.usuario(id number, name varchar(200), email varchar(200));
+
+CREATE SEQUENCE ULTRAHM.hibernate_sequence INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE SICREDIHM.hibernate_sequence INCREMENT BY 1 START WITH 1;
